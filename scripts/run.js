fcd5d01f06862e8505f6bdb0020c9866c942a9a2
alwin <alwin24012002@gmail.com>
2023-06-11 20:17:37 +0530

    Changes to test script

--- scripts/run.js
+++ scripts/run.js
@@ -1,14 +1,18 @@
 const hre = require('hardhat')
-const { utils } = require('ethers')
 
 async function main() {
 	const baseTokenURI = 'ipfs://QmZbWNKJPAjxXuNFSEaksCJVd1M6DaKQViJBYPK2BdpDEP/'
 
 	// Get owner/deployer's wallet address
-	const [owner] = await hre.ethers.getSigners()
+	const [owner, minter] = await hre.ethers.getSigners()
 
 	// Deploy contract with the correct constructor arguments
 	const contract = await hre.ethers.deployContract('NFTCollectible', [baseTokenURI])
+	// const contract = await hre.ethers.getContractAt(
+	// 	'NFTCollectible',
+	// 	'0x55166f74EE56A8C9b0E8505C111bE71d04924a60',
+	// 	minter
+	// )
 
 	// Wait for this transaction to be mined
 	await contract.waitForDeployment()
@@ -19,10 +23,10 @@ async function main() {
 	// Reserve NFTs
 	let txn = await contract.reserveNFTs()
 	await txn.wait()
-	console.log('10 NFTs have been reserved')
+	// console.log('10 NFTs have been reserved')
 
 	// Mint 3 NFTs by sending 0.03 ether
-	txn = await contract.mintNFTs(3, { value: hre.ethers.parseEther('0.03') })
+	txn = await contract.mintNFTs(2, { value: hre.ethers.parseEther('0.02') })
 	await txn.wait()
 
 	// Get all token IDs of the owner
